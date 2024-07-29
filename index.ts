926adbf14707689de9ad38c973c2553050afd945
kathir <kathir@centiizen.com>
2024-07-29 12:37:41 +0530

    removed console

--- index.ts
+++ index.ts
@@ -27,11 +27,7 @@ app.use("/.well-known/assetlinks.json", (req, res) => {
 app.post("/upload", upload.any(), async (req, res) => {
   console.log(req?.file, "req?.file");
 
-  if (req.file) {
-    uploadToCloud(req, res);
-  } else {
-    res?.status(404).json({ message: "File required!" });
-  }
+  uploadToCloud(req, res);
 });
 
 connectMongoDB();
