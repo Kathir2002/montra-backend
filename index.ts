3f53f761b691078df19a51d5e6a1599032acc544
kathir <kathir@centiizen.com>
2024-07-29 12:45:30 +0530

    changes

--- index.ts
+++ index.ts
@@ -5,10 +5,12 @@ config();
 import { authRouter } from "./src/routes/authRoute";
 import { connectMongoDB } from "./src/lib/connectDb";
 import assetLink from "./src/constant/assetlinks.json";
-import { OAuth2Client } from "google-auth-library";
+import cloud from "cloudinary";
 import { profileRouter } from "./src/routes/profileRoute";
 import { verifyToken } from "./src/middleware/verifyToken";
-import { upload, uploadToCloud } from "./src/config/upload";
+import multer from "multer";
+import path from "path";
+import fs from "fs";
 
 const app = express();
 app.use(express.json());
@@ -16,6 +18,25 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cors({ origin: "*" }));
 
 const port = process.env.PORT || 8000;
+
+const cloudinary = cloud.v2;
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+// Configure Multer
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "uploads/");
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + path.extname(file.originalname)); // Append the file extension
+  },
+});
+
+export const upload = multer({ storage: storage });
 
 app.use("/api/auth", authRouter);
 app.use("/api/user", verifyToken, profileRouter);
@@ -26,8 +47,27 @@ app.use("/.well-known/assetlinks.json", (req, res) => {
 
 app.post("/upload", upload.any(), async (req, res) => {
   console.log(req?.file, "req?.file");
+  // Upload the file to Cloudinary
+  if (req.file) {
+    // Upload the file to Cloudinary
+    cloudinary.uploader.upload(
+      req?.file?.path,
+      { folder: "your_folder_name" },
+      (error, result) => {
+        if (error) {
+          return res.status(500).send(error);
+        }
 
-  uploadToCloud(req, res);
+        // Delete the file from the local filesystem
+        fs.unlinkSync(req?.file?.path!);
+
+        // Respond with the URL of the uploaded file
+        res.json({ imageUrl: result?.secure_url, publicId: result?.public_id });
+      }
+    );
+  } else {
+    res.status(400).json({ error: "No file uploaded" });
+  }
 });
 
 connectMongoDB();
