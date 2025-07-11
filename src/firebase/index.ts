081e1e19b9cdae076d24e5450e4fc03fd317c8b0
kathir <kathirmthvn@gmail.com>
2025-07-11 17:37:08 +0530

    Refactor Firebase initialization to use environment variable for service account key
    - Added dotenv configuration to load environment variables.
    - Replaced hardcoded service account import with JSON parsed from FIREBASE_ADMIN_KEY environment variable.

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -1,7 +1,12 @@
+import { config } from "dotenv";
 import firebase, { ServiceAccount } from "firebase-admin";
-import serviceAccount from "../../montra-dev-429606-firebase-adminsdk-opuej-f234f508aa.json";
+config();
+
+// import serviceAccount from "../../montra-dev-429606-firebase-adminsdk-opuej-f234f508aa.json";
+const serviceAccount = JSON.parse(process.env.FIREBASE_ADMIN_KEY!) as ServiceAccount;
+
 firebase?.initializeApp({
-  credential: firebase.credential.cert(serviceAccount as ServiceAccount),
+  credential: firebase.credential.cert(serviceAccount),
 });
 
 export { firebase };
