62afffca9b277860358d830541c0034825ba00c9
kathir <kathirmthvn@gmail.com>
2025-07-12 17:49:38 +0530

    chore: update Node.js version to 20 and firebase-admin to 13.4.0; fix type assertion for service account

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -3,10 +3,10 @@ import firebase, { ServiceAccount } from "firebase-admin";
 config();
 
 // import serviceAccount from "../../montra-dev-429606-firebase-adminsdk-opuej-f234f508aa.json";
-const serviceAccount = JSON.parse(process.env.FIREBASE_ADMIN_KEY!) as ServiceAccount;
+const serviceAccount = JSON.parse(process.env.FIREBASE_ADMIN_KEY!);
 
 firebase?.initializeApp({
-  credential: firebase.credential.cert(serviceAccount),
+  credential: firebase.credential.cert(serviceAccount as ServiceAccount),
 });
 
 export { firebase };
